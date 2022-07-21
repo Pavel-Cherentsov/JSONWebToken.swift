0986c95fbe9bc3c6ac8265a5b1d88467108d71cb
Pavel <pcherentcov@twilio.com>
2022-07-21 16:29:31 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,22 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
   name: "JWT",
+  platforms: [
+    .macOS(.v10_12), .iOS(.v12)
+  ],
+  products: [
+    .library(
+      name: "JSONWebToken",
+      targets: ["JWT"]
+    )
+  ],
   dependencies: [
-    .Package(url: "https://github.com/krzyzanowskim/CryptoSwift", versions: Version(0, 6, 0) ..< Version(0, 7, 0))
+    .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMajor(from: "1.5.1"))
+  ],
+  targets: [
+    .target(name: "JWT", dependencies: ["CryptoSwift"]),
+    .testTarget(name: "JWTTests", dependencies: ["JWT"]),
   ]
 )
